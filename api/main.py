76a00fbd03a2621f718008fec38023b37fc4f844
Martigol2 <martignon@gmail.com>
2026-07-20 22:25:57 +0200

    Enable CORS for frontend

--- api/main.py
+++ api/main.py
@@ -1,9 +1,21 @@
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI, Body
 from utils import load_model
 from functions import predict_sales
 import pandas as pd
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+        "https://deployment-challenge.onrender.com",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 model = load_model("models/xgboost.pkl")
 scaler = load_model("models/scaler.pkl")
