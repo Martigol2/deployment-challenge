1b1356b9cf70ad9b7bd6dfb1d7230fd3400a2668
Martigol2 <martignon@gmail.com>
2026-07-20 22:39:34 +0200

    Allow Vercel frontend

--- api/main.py
+++ api/main.py
@@ -10,7 +10,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://deployment-challenge.onrender.com",
+        "https://deployment-challenge.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
